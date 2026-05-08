f7536b121e05fdfca663f16b32d4572d85ed634d
jmj367 <wakazu1106@gmail.com>
2026-05-08 15:48:41 +0900

    Revert "SDLに強く依存してそうな部分を削除"
    This reverts commit 7659d44ff710dde8692444ad8d652a25e50ca30f.

--- GameCore/Include/Texture.h
+++ GameCore/Include/Texture.h
@@ -16,21 +16,27 @@ public:
 	~Texture();
 
 	// 読込・解放
-	virtual bool Load(const std::string &fileName);
-	virtual void Unload();
+	bool Load(const std::string &fileName);
+	void Unload();
 
 	// レンダリング用テクスチャの作成
 	// サブカメラ等用
 	void CreateForRendering(int width, int height, unsigned int format);
 
+	void SetActive(int index = 0);
+
 	// ゲッター
 	int GetWidth() const { return mWidth; }
 	int GetHeight() const { return mHeight; }
+	unsigned int GetTextureID() const { return mTextureID; }
 	const std::string &GetFileName() const { return mFileName; }
 
 private:
 	// ファイル名
 	std::string mFileName;
+
+	// テクスチャID
+	unsigned int mTextureID;
 
 	// テクスチャの幅・高さ
 	int mWidth;
